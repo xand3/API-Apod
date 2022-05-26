ac68343bcee8e30437eda6e1b355c65e4a75d413
Alexandre Bastos <xande.bastos321@gmail.com>
2022-05-26 11:01:57 -0300

    Feita a função para definir URL da imagem gerada pela API

--- JS/main.js
+++ JS/main.js
@@ -4,15 +4,25 @@ function fazGet(url) {
     request.send()
     return request.responseText
 }
+let img = document.getElementById("img-apod")
 
-function criaLinha(usuario) {
-
+function defineUrl(id, url) {
+    if(id == 'img-apod') {
+        img.setAttribute("src", url)
+    }
 }
 
+
+let info
 function main() {
     data = fazGet("https://api.nasa.gov/planetary/apod?api_key=HDQhgkx4GJeyle6mxaeoirveFKMwkOppul5Lhsgo")
-    usuarios = JSON.parse(data)
-    console.log(usuarios)
+    info = JSON.parse(data)
+    console.log(info)
+
+    console.log(info.hdurl)
+
+    
 }
 
 main()
+defineUrl("img-apod", info.url)
