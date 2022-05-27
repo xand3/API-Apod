ea326cb2278c384fdef2493e9cc70a61fa819180
Alexandre Bastos <84591023+xand3@users.noreply.github.com>
2022-05-27 11:34:16 -0300

    Tratando media_type video e imagem

--- JS/main.js
+++ JS/main.js
@@ -7,9 +7,17 @@ $("#send").click(async function(event) {
         let dados = await fetch("https://api.nasa.gov/planetary/apod?api_key=HDQhgkx4GJeyle6mxaeoirveFKMwkOppul5Lhsgo&date=" + data)
         let dadosConvertidos = await dados.json()
         
-        $("#img-apod").attr("src", dadosConvertidos.url) 
-        $("#title").text(dadosConvertidos.title)
-        $("#explanation").text(dadosConvertidos.explanation)
+        if(dadosConvertidos.media_type = "image") {
+            $("#img-apod").attr("src", dadosConvertidos.url) 
+            $("#title").text(dadosConvertidos.title)
+            $("#explanation").text(dadosConvertidos.explanation)
+            $("video").hide()
+        } else if (dadosConvertidos.media_type = "video") {
+            $("#vid-apod").attr("src", dadosConvertidos.url)
+            $("#title").text(dadosConvertidos.title)
+            $("#explanation").text(dadosConvertidos.explanation)
+            $("img").hide()
+        }
     }
     $(".form").hide()
 })
